d01ab873368cac9b5edede9124259a28001a0513
Flopiras <Flopiras@yahoo.it>
2023-06-09 15:13:04 +0200

    get element by DOM

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('js ok');
+
+//elementi del DOM
+const startButton = document.getElementById('start-button');
+const countdown =  document.getElementById('countdown');
+const randomNumbers =  document.getElementById('random-numbers');
+const card =  document.getElementById('number-card');
+const userNumber1 =  document.getElementById('n1');
+const userNumber2 =  document.getElementById('n2');
+const userNumber3 =  document.getElementById('n3');
+const userNumber4 =  document.getElementById('n4');
+const userNumber5 =  document.getElementById('n5');
+const inputButton =  document.getElementById('input-button');
+
+
+
+
